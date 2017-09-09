5311f4369b04ee1bda89444ecf54bca0bbc00a7e
Marek <marek.marchlewicz@gmail.com>
2017-09-10 01:16:06 +0800

    Added lookAt

---
+++ Assets/Scripts/LookAt.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAt : MonoBehaviour
+{
+    [SerializeField]
+    private Transform boids;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector3 centre = Vector3.zero;
+		foreach(Transform t in boids.GetComponentsInChildren<Transform>())
+        {
+            centre += t.position;
+        }
+        centre /= boids.GetComponentsInChildren<Transform>().Length;
+
+
+        transform.LookAt(centre);
+    }
+}
